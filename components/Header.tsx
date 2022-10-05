f3efa410be15891f98dc111bb66ac1319b8e893e
Nde Lucien <chelucien08@gmail.com>
2022-10-05 16:32:13 +0100

    completed header

---
+++ components/Header.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import ImageLoader from "../image_loader";
+import logo from "../public/logo.png";
+import { SearchIcon, BellIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+
+export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          src={logo}
+          alt="logo"
+          loader={ImageLoader}
+          height={70}
+          width={150}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden sm:inline  h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="hidden sm:inline  h-6 w-6" />
+        <Link href="/account" className="ml-4">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="account logo"
+            width={35}
+            height={35}
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["rickandmortyapi.com, rb.gy"],
+    loader: "custom",
+    path: "/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
