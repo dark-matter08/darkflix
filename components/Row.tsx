239db927e1c9be1eea4cdb5c90b3c3e89cd2d606
Nde Lucien <chelucien08@gmail.com>
2022-10-06 13:08:03 +0100

    commit

--- components/Row.tsx
+++ components/Row.tsx
@@ -12,7 +12,7 @@ interface Props {
 export const Row = ({ title, movies }: Props) => {
   const rowRef = useRef<HTMLDivElement>(null);
   return (
-    <div className="h-40 space-y-0.5">
+    <div className="space-y-0.5">
       <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl ">
         {title}
       </h2>
@@ -20,7 +20,7 @@ export const Row = ({ title, movies }: Props) => {
         <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
         <div
           ref={rowRef}
-          className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 scrollbar-hide"
+          className="flex items-center space-x-1 overflow-x-scroll overflow-y-hidden md:space-x-2.5 scrollbar-hide"
         >
           {movies.map((movie) => {
             return <Thumbnail key={movie.id} movie={movie} />;
